600165fedee9c0cfc035f5abfc04b8d02306b406
victormeloasm <victormeloasm@gmail.com>
2023-06-18 04:11:28 -0300

    Update Trillion.c

--- Trillion.c
+++ Trillion.c
@@ -2,8 +2,8 @@
 #include <stdint.h>
 #include <pthread.h>
 
-#define THREAD_COUNT 32  // Number of threads to use, my CPU is a AMD Ryzen 9 5950X so it's does have 32 threads. 
-#define TARGET_COUNT 1000000000000ULL  // 1 trillion
+#define THREAD_COUNT 32
+#define TARGET_COUNT 1000000000000ULL // 1 trillion
 
 typedef struct {
     uint64_t start;
@@ -15,12 +15,20 @@ void* countRange(void* arg) {
     ThreadData* data = (ThreadData*)arg;
     uint64_t count = 0;
 
-    // Perform counting using simple loop
+    // Perform counting within the assigned range, up to 1 trillion
     uint64_t counter = data->start;
     uint64_t increment = 8ULL;
+    uint64_t targetCount = data->end;
 
-    while (counter <= data->end && counter <= TARGET_COUNT) {
-        ++count;
+    // Unroll the loop to process multiple iterations at once
+    while (counter <= targetCount - 8) {
+        count += 8;
+        counter += increment * 8;
+    }
+
+    // Process the remaining iterations
+    while (counter <= targetCount) {
+        count += 1;
         counter += increment;
     }
 
@@ -33,11 +41,23 @@ int main() {
     ThreadData threadData[THREAD_COUNT];
     uint64_t totalCount = 0ULL;
 
-    uint64_t rangeSize = TARGET_COUNT / THREAD_COUNT;
+    uint64_t rangeSize = (TARGET_COUNT + THREAD_COUNT - 1) / THREAD_COUNT;
+    uint64_t remainingCount = TARGET_COUNT % THREAD_COUNT;
+    uint64_t currentStart = 1ULL;
+
     for (int i = 0; i < THREAD_COUNT; ++i) {
-        threadData[i].start = i * rangeSize + 1ULL;
-        threadData[i].end = (i + 1) * rangeSize;
+        threadData[i].start = currentStart;
+        threadData[i].end = currentStart + rangeSize - 1ULL;
+
+        // Distribute the remaining count among the first few threads
+        if (remainingCount > 0) {
+            threadData[i].end++;
+            remainingCount--;
+        }
+
         pthread_create(&threads[i], NULL, countRange, (void*)&threadData[i]);
+
+        currentStart = threadData[i].end + 1ULL;
     }
 
     for (int i = 0; i < THREAD_COUNT; ++i) {
